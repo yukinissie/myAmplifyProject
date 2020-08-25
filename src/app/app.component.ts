9e0e905492587d1e51fbcc8f1008276d5ba0b79c
yukinissie <bigtree2490@outlook.jp>
2020-08-26 08:14:19 +0900

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,21 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { APIService } from './API.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  todos: Array<any>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private apiService: APIService
   ) {
     this.initializeApp();
   }
@@ -22,6 +27,20 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.apiService.ListTodos().then((evt) => {
+        this.todos = evt.items;
+      });
+      this.apiService.OnCreateTodoListener.subscribe((evt) => {
+        const data = (evt as any).value.data.onCreateTodo;
+        this.todos = [...this.todos, data];
+      });
+    });
+  }
+
+  createTodo() {
+    this.apiService.CreateTodo({
+      name: 'ionic',
+      description: 'testing'
     });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import PubSub from '@aws-amplify/pubsub';
+import API from '@aws-amplify/api';
+import awsconfig from './aws-exports';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
